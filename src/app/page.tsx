ca013d46d1c9f23a447430378f4249ad393bc8ff
unknown <rahul66.ugcs20@iiitranchi.ac.in>
2024-08-14 19:18:07 +0530

    remove authorization check from home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,22 +7,8 @@ import Navbar from "./components/navbars/navbar";
 import TotalCost from "./components/totalCost";
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import { useRouter } from "next/navigation";
 
 export default function Home() {
-  const router = useRouter()
-  const { addToken} = tokenStore((state)=>({
-    addToken:state.addToken
-  }))
-  
-  useEffect(()=>{
-    const token = localStorage.getItem("token")
-    if(!token){
-      router.push("/admin/login")
-    }
-    addToken(token)
-
-  },[])
   return (
     <div>
         <Navbar type={"customer"} />
