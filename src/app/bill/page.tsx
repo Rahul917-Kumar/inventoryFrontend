4f3d41fef9f5584515bc7c0c9f0e582f8300cbbe
unknown <rahul66.ugcs20@iiitranchi.ac.in>
2024-08-15 01:52:12 +0530

    remove login check from bill page

--- src/app/bill/page.tsx
+++ src/app/bill/page.tsx
@@ -10,41 +10,18 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import { Button, Divider, Tooltip, Typography } from '@mui/material'
-import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 import { useRouter } from 'next/navigation'
 import 'react-toastify/dist/ReactToastify.css';
 import Navbar from '../components/navbars/navbar'
 import VerifiedIcon from '@mui/icons-material/Verified';
-import tokenStore from '../../../store/tokenStore'
-import { clear } from 'console'
 
 const CartPage = () => {
     const router = useRouter()
-    const {token, addToken} = tokenStore((state)=>({
-        token:state.token,
-        addToken:state.addToken
-      }))
-
-    const {totalAmount, items, clearAmount, clearItems, billAmount, billItems} = itemStore((state)=>({
-        totalAmount:state.totalAmount,
-        items:state.items,
-        clearAmount:state.clearAmount,
-        clearItems:state.clearItems,
+    const { billAmount, billItems} = itemStore((state)=>({
         billAmount:state.billAmount,
         billItems:state.billItems
       }))
 
-      
-    useEffect(()=>{
-        const token = localStorage.getItem("token")
-        if(!token){
-        router.push("/admin/login")
-        }
-        if(billAmount===0){
-            router.push("/admin")
-        }
-        addToken(token)
-    },[])
 
         return (
             <>
